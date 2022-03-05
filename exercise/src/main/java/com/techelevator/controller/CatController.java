14a4b57a065a1cef399cb0166897e9c96427df0c
read -r -p "Enter your email: " email <>
2022-03-05 10:54:39 -0500

    review

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -1,8 +1,14 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class CatController {
 
@@ -15,6 +21,27 @@ public class CatController {
         this.catFactService = catFactService;
         this.catPicService = catPicService;
     }
+
+    //GET /api/cards: Provides a list of all Cat Cards in the user's collection.
+
+    @RequestMapping(path = "/cards" , method = RequestMethod.GET)
+    public List<catCard> getAll(){
+
+        return catCardDao.list();
+
+    }
+
+    ///POST /api/cards: Saves a card to the user's collection.
+
+//    @ResponseStatus (HttpStatus.CREATED)
+//    @RequestMapping(path = "/cards" , method = RequestMethod.POST)
+//            public boolean addCard (@RequestBody CatCard catCard)
+//    {
+//
+//        return catCardDao.save(catCard);
+//    }
+
+
 
 
 }
